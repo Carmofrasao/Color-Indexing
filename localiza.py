ee964d67877199b3331cf63ae09060b5451e25f2
Anderson <anderfrasao52@gmail.com>
2023-05-08 21:47:29 -0300

    ..

--- localiza.py
+++ localiza.py
@@ -96,25 +96,32 @@ for filename in os.listdir(img_dir):
 
 # abre a imagem de consulta em formato JPEG
 # query_image = Image.open('carte.jpg')
+# sim = 0.55
 # query_image = Image.open('chave.jpg')
+# sim = 0.45
 # query_image = Image.open('penal.jpg')
+# sim = 0.5
 # query_image = Image.open('proc.jpg')
-# query_image = Image.open('livro.jpg')
-query_image = Image.open('caneca.jpg')
+# sim = 0.42
+query_image = Image.open('livro.jpg')
+sim = 0.7
+# query_image = Image.open('caneca.jpg')
+# sim = 0.84
+
+num_images = 12
 
 # converte a imagem para uma matriz numpy
 query_image = np.asarray(query_image)
 
 ordered_images, sim_list = color_indexing(database=images, query=query_image, bin_size=16)
 
-num_images = 12
-
 fig, axs = plt.subplots(4, 3, figsize=(12, 8))
 for i in range(num_images):
-    row = i // 3
-    col = i % 3
-    axs[row, col].imshow(ordered_images[i])
-    axs[row, col].set_title(f"Similarity: {sim_list[i]:.2f}")
-    axs[row, col].axis('off')
+    if sim_list[i] >= sim:
+        row = i // 3
+        col = i % 3
+        axs[row, col].imshow(ordered_images[i])
+        axs[row, col].set_title(f"Similarity: {sim_list[i]:.2f}")
+        axs[row, col].axis('off')
 
 plt.show()
